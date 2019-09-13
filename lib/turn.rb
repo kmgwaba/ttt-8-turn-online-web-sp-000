67b49151a327c4ebe669e38f24d5de22209769da
Kathia Mgwaba <mgwaba68@gmail.com>
2019-09-13 14:21:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,9 @@
 def display_board(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
+  puts "-----------"
+  puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
+  puts "-----------"
+  puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end
 
 def valid_move?(board, index)
@@ -25,4 +29,4 @@ def turn(board)
 end
 
 def move?(board, index)
-end 
+end
